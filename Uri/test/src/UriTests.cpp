425090dad7243335e56fbdf9fb80480c19eb9d61
lamductan <lamductan@gmail.com>
2019-10-27 18:49:42 +0700

    initial project

---
+++ Uri/test/src/UriTests.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file UriTests.cpp
+ * 
+ * This module contains the unit tests of the Uri::Uri class
+ */
+
+#include <gtest/gtest.h>
+#include <Uri/Uri.hpp>
+
+TEST(UriTests, Placeholder)
+{
+  Uri::Uri uri;
+  ASSERT_TRUE(true);
+}
